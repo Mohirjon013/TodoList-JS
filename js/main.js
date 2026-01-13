bb161252dc2eaaa6ae31b460eaee78c0f194c1c7
Mohirjon013 <mohirjon26@gmail.com>
2026-01-13 10:27:20 +0500

    improve code quality and fix bugs

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ let elCancelBtn = document.querySelector(".cancel-btn")
 
 let elAllCount = document.querySelector(".all-count")
 let elUnCompletedCount = document.querySelector(".unCompleted-count")
-let eIIsCompletedCount = document.querySelector(".isCompleted-count")
+let elIsCompletedCount = document.querySelector(".isCompleted-count")
 
 let elChoosenImg = document.querySelector(".choosen-img")
 let elUploadedImg = document.querySelector(".uploaded-img")
@@ -21,20 +21,24 @@ let todo = JSON.parse(localStorage.getItem("setTodo")) || []
 
 elForm.addEventListener("submit", function(e){
     e.preventDefault() // refreshi oldni oladi
+    
+    if(elInput.value.trim() === "") {
+        alert("Please enter a todo item!")
+        return
+    }
     const data = {
-        id:todo.length + 1,
+        id:Date.now(),
         value:elInput.value,
-        isComplated:false,
-        UnComplated:true ,
+        isCompleted:false,
+        UnCompleted:true ,
         imgURL: e.target.choosenImg.files[0] ? URL.createObjectURL(e.target.choosenImg.files[0]) : null
     }
     e.target.reset()
     todo.push(data)
     renderTodo(todo)
-    elUploadedImg.src = "./images/uplaod-icon.svg"
+    elUploadedImg.src = "./images/uploaded-img.jpg"
     localStorage.setItem("setTodo", JSON.stringify(todo))
 })
-
 
 
 // Rendering start 
@@ -42,33 +46,33 @@ function renderTodo(arr){
     elTodoList.innerHTML = null
     arr.forEach((item, index) => {
         let elTodoItem = document.createElement("li")
-        elTodoItem.className = `flex items-center gap-5 justify-between p-[13px] border border-[#D9D9D9] ${item.isComplated ? "opacity-50" : ""} `
+        elTodoItem.className = `flex items-center gap-5 justify-between p-[13px] border border-[#D9D9D9] ${item.isCompleted ? "opacity-50" : ""} `
         elTodoItem.innerHTML = `
-            <p class="text-[20px] font-normal">
-                <span class="font-semibold text-[14px] mr-[5px]">${index + 1}.</span>
-                <span class="${item.isComplated ? "line-through" : ""}">${item.value}</span>
-            </p>
-            
-            ${item.imgURL ? `<img src="${item.imgURL}" alt="uploaded-img" width="50" height="50">` : ""}
-            <div class="flex items-center gap-[10px]">
+            <div class="flex items-start gap-2 flex-1 ">
+                <span class="block font-black text-[14px] mt-[3px] flex-shrink-0">${index + 1}.</span>
+                <p class="text-[16px] block ${item.isCompleted ? "line-through" : ""} break-all flex-1">${item.value}</p>
+                ${item.imgURL ? `<div class=" flex-shrink-0 w-[70px] h-[50px] rounded overflow-hidden border border-gray-200"> <img class="w-full h-full object-cover " src="${item.imgURL}" alt="uploaded-img"> </div>` : ""}
+            </div>
+
+            <div class="max-w-[190px] flex items-center gap-[10px]">
                 <button id="${item.id}" type="button" class="delete-btn hover:scale-125 duration-300 cursor-pointer">
                     <img class="pointer-events-none" src="./images/delete-icon.svg" alt="delete-icon" width="20" height="20">
                 </button>
                 <button id="${item.id}" class="update-btn hover:scale-125 duration-300 cursor-pointer">
                     <img class="pointer-events-none" src="./images/update-icon.svg" alt="update-icon" width="20" height="20">
                 </button>
-                <button onclick="handleCompleteBtn(${item.id})"class="hover:scale-125 duration-300 cursor-pointer ${item.isComplated ? "hidden" : "block"}">
+                <button onclick="handleCompleteBtn(${item.id})"class="hover:scale-125 duration-300 cursor-pointer ${item.isCompleted ? "hidden" : "block"}">
                     <img src="./images/complete-icon.svg" alt="complete-icon" width="20" height="20">
                 </button>
-                <button onclick="handleCompleteBtn(${item.id})" class="w-[20px] h-[20px] bg-[#05FF00] rounded-full hover:scale-125 duration-300 cursor-pointer ${item.isComplated ? "block" : "hidden"}"></button>
+                <button onclick="handleCompleteBtn(${item.id})" class="w-[20px] h-[20px] bg-[#05FF00] rounded-full hover:scale-125 duration-300 cursor-pointer ${item.isCompleted ? "block" : "hidden"}"></button>
             </div>
         `
         elTodoList.appendChild(elTodoItem)
     });
     
     elAllCount.textContent = todo.length
-    elUnCompletedCount.textContent = todo.filter(item => item.UnComplated == true).length
-    eIIsCompletedCount.textContent = todo.filter(item => item.isComplated == true).length
+    elUnCompletedCount.textContent = todo.filter(item => item.UnCompleted == true).length
+    elIsCompletedCount.textContent = todo.filter(item => item.isCompleted == true).length
 }
 renderTodo(todo)
 // Rendering start 
@@ -89,29 +93,32 @@ elTodoList.addEventListener("click", function(e){
         const itemUpdatedId = e.target.id
         const findedUpdatedObj = todo.find(item => item.id == itemUpdatedId)
         
+
         elModalContent.innerHTML = `
-            <form class="update-form mb-[100px] max-w-[600px] px-[30px]">
+            <form class="update-form sm:max-w-[600px] max-w-[350px] sm:px-[30px] px-[20px]">
                 <label>
-                    <input class="w-full py-[11px] px-[15px] rounded-lg border-[2px] border-[#32A3FE] outline-none placeholder:text-[#32A3FE]" type="text" value="${findedUpdatedObj.value}" name="user_updated" placeholder="Update your note..." autocomplete="off">
+                    <input class="w-full sm:py-[11px] py-[5px] sm:px-[15px] px-[10px] rounded-lg border-[2px] border-[#32A3FE] outline-none placeholder:text-[#32A3FE]" type="text" value="${findedUpdatedObj.value}" name="user_updated" placeholder="Update your note..." autocomplete="off">
                 </label>
-                <label>
-                    <input class="hidden update-files" type="file">
-                    <img class="update-img rounded-md mt-6 mx-auto" src="${findedUpdatedObj.imgURL}" alt="updated-img" width="100" height="100">
+                <label for="update-file-input" class="block sm:mt-6 mt-4 cursor-pointer block">
+                    <input id="update-file-input" class="hidden update-files " type="file">
+                    <img class="update-img rounded-md sm:mt-6 mt-4 mx-auto cursor-pointer object-cover" src="${findedUpdatedObj.imgURL ? findedUpdatedObj.imgURL : './images/uploaded-img.jpg'}" alt="updated-img" width="100" height="100">
                 </label>
-                <div class="flex items-center justify-between mt-[100px]">
-                    <button onclick="handleCancelBtn()" class="w-[100px] py-[10px] rounded-lg hover:bg-[#32A3FE] hover:text-white duration-500 border-[2px] border-[#32A3FE] text-[#32A3FE] cursor-pointer" type="button">Cancel</button>
-                    <button class="w-[100px] py-[10px] rounded-lg hover:bg-transparent hover:text-[#32A3FE] duration-500 bg-[#32A3FE] border-[2px] border-[#32A3FE] text-white cursor-pointer" type="submit">Apply</button>
+                <div class="flex items-center justify-between sm:mt-[70px] mt-[50px]">
+                    <button onclick="handleCancelBtn()" class="sm:w-[100px] w-[80px] sm:py-[10px] py-[7px] rounded-lg hover:bg-[#32A3FE] hover:text-white duration-500 border-[2px] border-[#32A3FE] text-[#32A3FE] cursor-pointer" type="button">Cancel</button>
+                    <button class="sm:w-[100px] w-[80px] sm:py-[10px] py-[7px] rounded-lg hover:bg-transparent hover:text-[#32A3FE] duration-500 bg-[#32A3FE] border-[2px] border-[#32A3FE] text-white cursor-pointer" type="submit">Apply</button>
                 </div>
             </form>        
         `
         let elUpdateForm = document.querySelector(".update-form")
         let elUpdateImg = document.querySelector(".update-img")
         let elUpdateFiles = document.querySelector(".update-files")
-
+        
         elUpdateFiles.addEventListener("change", function(e){
-            elUpdateImg.src = URL.createObjectURL(e.target.files[0])
+            if(e.target.files[0]){
+                elUpdateImg.src = URL.createObjectURL(e.target.files[0])
+            }
         })
-
+        
         elUpdateForm.addEventListener("submit", function(e){
             e.preventDefault()
             
@@ -135,18 +142,17 @@ elWrapperModal.addEventListener("click", function(e){
         document.body.classList.remove("overflow-y-hidden")
     }
 })
-
 function handleCancelBtn(){
     elWrapperModal.classList.add("scale-0")
     document.body.classList.remove("overflow-y-hidden")
 }
 // Updated open and close end 
 
-// isComplated start 
+// isCompleted start 
 function handleCompleteBtn(id){
     const findedCompleteObj = todo.find(item => item.id == id)
-    findedCompleteObj.isComplated = !findedCompleteObj.isComplated
-    findedCompleteObj.UnComplated = !findedCompleteObj.isComplated    
+    findedCompleteObj.isCompleted = !findedCompleteObj.isCompleted
+    findedCompleteObj.UnCompleted = !findedCompleteObj.isCompleted    
     renderTodo(todo)
     localStorage.setItem("setTodo", JSON.stringify(todo))  
 }
@@ -155,19 +161,17 @@ function handleAllbox(){
     renderTodo(todo)   
 }
 function handleUnCompletedBox(){
-    const filteredUncompletedArr = todo.filter(item => item.UnComplated == true)
+    const filteredUncompletedArr = todo.filter(item => item.UnCompleted == true)
     renderTodo(filteredUncompletedArr)
-    localStorage.setItem("setTodo", JSON.stringify(todo))
-    
 }
 function handleIsCompletedBox(){
-    const eIIsCompletedCount = todo.filter(item => item.isComplated == true)
-    renderTodo(eIIsCompletedCount)
-    localStorage.setItem("setTodo", JSON.stringify(todo)) 
+    const elIsCompletedCount = todo.filter(item => item.isCompleted == true)
+    renderTodo(elIsCompletedCount)
 }
-// isComplated start 
+// isCompleted start 
+
 
 elChoosenImg.addEventListener("change", function(e){
-    changeURL = URL.createObjectURL(e.target.files[0])
+    let changeURL = URL.createObjectURL(e.target.files[0])
     elUploadedImg.src = changeURL
 })
